fcbeeb56b8779b782d857cbce71dee2b61528e7d
Brandon Hernandez <brandon.hernandez@itksquare.edu.mx>
2023-03-03 02:45:37 -0600

    additional questions

--- back/database/data.js
+++ back/database/data.js
@@ -1,20 +1,22 @@
 export default [
     {
         id: 1,
-        question : "Javascript is an _______ language",
+        question : "Which keyword should be used to define a value that should not change in the application?",
         options : [
-            'Object-Oriented',
-            'Object-Based',
-            'Procedural',
+            'var',
+            'let',
+            'const',
+            'boolean',
         ]
     },
     {
         id: 2,
-        question : "Following methods can be used to display data in some form using Javascript",
+        question : "What type of coercion is automatically performed",
         options : [
-            'document.write()',
-            'console.log()',
-            'window.alert()',
+            'explicit',
+            'implicit',
+            'declared',
+            'expressive',
         ]
     },
     {
@@ -24,26 +26,79 @@ export default [
             'Boolean',
             'Undefined',
             'Object',
+            'NaN'
         ]
     },
     {
         id: 4,
-        question : "What does the toString() method return?",
+        question : "Symbol used to create an array",
         options : [
-            'Return Object',
-            'Return String',
-            'Return Integer'
+            '- -',
+            '[]',
+            '{}',
+            '//'
         ]
     },
     {
         id: 5,
+        question : "keyword used to create a class that takes properties from another previously defined class",
+        options : [
+            'takes',
+            'accepts',
+            'extends',
+            'super',
+        ]
+    },
+    {
+        id: 6,
+        question : "Which function is used to deserialize an object into a JSON string?",
+        options : [
+            'stringify()',
+            'parse()',
+            'convert()',
+            'deserialize()',
+        ]
+    },
+    {
+        id: 7,
+        question : "What data type does true/false fall into",
+        options : [
+            'String',
+            'Number',
+            'bigInt',
+            'boolean'
+        ]
+    },
+    {
+        id: 8,
+        question : "What does the logical operator || stand for?",
+        options : [
+            'Not',
+            'double Not',
+            'Or',
+            'And'
+        ]
+    },
+    {
+        id: 9,
         question : "Which function is used to serialize an object into a JSON string?",
         options : [
             'stringify()',
             'parse()',
             'convert()',
+            'deserialize()'
+        ]
+    },
+    {
+        id: 10,
+        question : "aside from dot notation what other method can be used to access an object property",
+        options : [
+            'bracket notation',
+            'curly braces notation',
+            'only dot notation can be used',
+            'parenthesis notation',
         ]
     }
 ];
 
-export const answers = [0, 1, 2, 1, 0];
+export const answers = [2, 1, 2, 1, 2, 0, 3, 2, 1, 0];
